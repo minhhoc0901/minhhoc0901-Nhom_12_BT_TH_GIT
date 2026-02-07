7f485840ec7af5d8ec875b2da7456108bb330a35
letoanban <letoanban.word@gmail.com>
2026-02-07 13:02:12 +0700

    refactor: clean up add-book logic

--- action.js
+++ action.js
@@ -119,7 +119,7 @@ async function SearchBook(event) {
 
 //Thêm Sách
 
-function AddBook(event) {
+async function AddBook(event) {
   event.preventDefault();
   const form = document.getElementById("addBookForm");
   const book = {
@@ -130,35 +130,37 @@ function AddBook(event) {
     category: form.category.value,
     image: "",
   };
+
   if (form.image.files.length > 0) {
     book.image = form.image.files[0].name;
   }
 
-  fetch("http://localhost:3000/books", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(book),
-  })
-    .then((res) => {
-      if (!res.ok) {
-        throw new Error("Thêm sách thất bại");
-      }
-      return res.json();
-    })
-    .then((data) => {
-      console.log("dahsjdhasjkdhkjasd", data);
-      alert("Thêm sách thành công!");
-      form.reset();
-      const modal = bootstrap.Modal.getInstance(
-        document.getElementById("addBookModal"),
-      );
-      modal.hide();
-      console.log("Book mới:", data);
-    })
-    .catch((err) => {
-      alert("Có lỗi xảy ra");
-      console.error(err);
+  try {
+    const res = await fetch("http://localhost:3000/books", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(book),
     });
+
+    if (!res.ok) {
+      throw new Error("Thêm sách thất bại");
+    }
+
+    const data = await res.json();
+    console.log("Book mới:", data);
+
+    alert("Thêm sách thành công!");
+    form.reset();
+
+    // Ẩn modal
+    const modal = bootstrap.Modal.getInstance(
+      document.getElementById("addBookModal"),
+    );
+    modal.hide();
+  } catch (err) {
+    alert("Có lỗi xảy ra");
+    console.error(err);
+  }
 }
